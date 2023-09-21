a4c0a7205409734cc983bce75c55cb8600976401
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-09-21 14:11:23 -0400

    page confnig

---
+++ components/config/config.tsx
@@ -0,0 +1,20 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+    name: "Mutuals",
+    description:
+            "Your weekend plans",
+    mainNav: [
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Secret",
+            href: "/secret",
+        }
+    ],
+    links: {
+        github: "https://github.com/karam-khanna/project-apollo",
+    },
+}
