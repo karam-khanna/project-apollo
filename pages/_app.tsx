9ae28f8f7c14f5b5ed021e379d9677eff84ba653
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-09-21 14:12:09 -0400

    add header to default apge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {Head} from "next/document";
+import {ThemeProvider} from "@/components/theme-provider";
+import {SiteHeader} from "@/components/site-header";
+import {Toaster} from "@/components/ui/toaster";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+            <ThemeProvider attribute="class" defaultTheme="dark">
+                <div className="relative flex min-h-screen flex-col">
+                    <SiteHeader/>
+                    <div className="flex-1">
+                        <Component {...pageProps} />
+                        <Toaster/>
+                    </div>
+                </div>
+            </ThemeProvider>
+
+    )
 }
