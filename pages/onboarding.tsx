1d6fa4b20c1f442c5e5fb2db6bee7524dcee9842
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-10-02 14:06:36 -0400

    updating onboarding

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -2,7 +2,7 @@ import React, {useContext, useEffect, useState} from "react";
 import {UserContext} from "@/context/UserContext";
 import {Button} from "@/components/ui/button";
 import {LucideHelpCircle, LucideRefreshCcw, LucideSettings} from "lucide-react";
-// import {OnboardingForm} from "@/components/onboarding/form/onboarding-form";
+import {OnboardingForm} from "@/components/onboarding/form/onboarding-form";
 import {Card, CardContent, CardDescription} from "@/components/ui/card";
 import useMobileScreen from "@/components/ui/isMobile";
 
@@ -11,6 +11,7 @@ export default function Onboarding() {
     // context variables
     const {userAuth, setUserAuth} = useContext(UserContext);
     const {user, setUser} = useContext(UserContext);
+    const [isLoading, setIsLoading] = useState(false);
     // If the primaryColor is in HSL format, convert it to Hex.
     return (
             <div>
@@ -19,7 +20,7 @@ export default function Onboarding() {
                     <h1 className={"text-2xl sm:text-5xl font-semibold pt-7 sm:pt-16"}>Welcome to Mutuals
                         🚀</h1>
                     <Card className={"mt-8 sm:mt-16 w-11/12 sm:w-1/2 p-3 sm:p-6"}>
-                        {/*<OnboardingForm setIsLoading={setIsLoading}/>*/}
+                        <OnboardingForm setIsLoading={setIsLoading}/>
                     </Card>
                     {/*</div>*/}
                 </div>
