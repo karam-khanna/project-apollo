0e69b1b3ef1cd42f1dbc29f6365eb0b254cc7563
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-10-02 12:11:25 -0400

    reformatting

--- components/firebase.tsx
+++ components/firebase.tsx
@@ -1,22 +1,33 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import {getAnalytics} from "firebase/analytics";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
+import {FirebaseApp, getApps, initializeApp} from 'firebase/app';
+import {Firestore, getFirestore} from "firebase/firestore";
+import {Auth} from "@firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-5QHy3cJupg8U9p85rKQ5qQsJTbkFMBE",
-  authDomain: "mutuals---devkam.firebaseapp.com",
-  projectId: "mutuals---devkam",
-  storageBucket: "mutuals---devkam.appspot.com",
-  messagingSenderId: "750997161836",
-  appId: "1:750997161836:web:c8cb6506db6aae0e5b5549",
-  measurementId: "G-YZC2Z7VJCT"
+    apiKey: "AIzaSyC-5QHy3cJupg8U9p85rKQ5qQsJTbkFMBE",
+    authDomain: "mutuals---devkam.firebaseapp.com",
+    projectId: "mutuals---devkam",
+    storageBucket: "mutuals---devkam.appspot.com",
+    messagingSenderId: "750997161836",
+    appId: "1:750997161836:web:c8cb6506db6aae0e5b5549",
+    measurementId: "G-YZC2Z7VJCT"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const firebase_app: FirebaseApp = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const provider: GoogleAuthProvider = new GoogleAuthProvider();
+
+const firebase_auth: Auth = getAuth(firebase_app);
+const db: Firestore = getFirestore(firebase_app);
+
+
+export {firebase_auth, provider, db, firebase_app}
+
+
