0e0250fa1e562dc233482c5aed0cc5bcf7255c6f
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-10-11 12:47:49 -0700

    filler shell and text

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -5,7 +5,11 @@ const inter = Inter({subsets: ['latin']})
 export default function Home() {
     return (
             <div>
-
+                <div className={"flex flex-col items-center justify-center pt-16 gap-9"}>
+                    <h1 className="text-6xl font-bold text-center">Secret Page!</h1>
+                    <p>Here we will put admin stuff. Later will add some rules here so only admins can visit this page
+                        and take it off the header</p>
+                </div>
             </div>
 
     )
