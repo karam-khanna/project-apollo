12f33ca1cdfdc94a883b09e893fdc5f4e44439f1
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-10-11 13:29:59 -0700

    use router not href

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import {Inter} from 'next/font/google'
 import {Button} from "@/components/ui/button";
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 import {
     Card,
     CardContent,
@@ -13,27 +13,21 @@ import {
 } from "@/components/ui/card"
 import {Label} from "@/components/ui/label"
 import {ThemeToggle} from "@/components/theme-toggle";
+import {useContext} from "react";
+import {UserContext} from "@/context/UserContext";
 
 const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
+    const {user, setUser} = useContext(UserContext);
+    const router = useRouter();
     return (
             <div className={"flex flex-col items-center justify-center pt-16 gap-9"}>
                 <h1 className="text-6xl font-bold text-center">Welcome to Mutuals!</h1>
                 <div className={"flex flex-col items-center justify-center gap-2"}>
-                    {/* <Button onClick={
-                        () => {
-                            console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-                            console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-                            console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-                            console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-                        }
-                    }> Click me</Button>
-                    <h3 className={"text-2xl font-medium"}>Check console for result</h3> */}
-
-                <Button onClick={() => window.location.href = '/login'}>Log In</Button>
-                <Button onClick={() => window.location.href = '/signup'}>Sign Up</Button>
-                <Button onClick={() => window.location.href = '/calendarpage'}>Calendar Page</Button>
+                    <Button onClick={() => router.push('/login').then()}>Log In</Button>
+                    <Button onClick={() => router.push('/signup').then()}>Sign Up</Button>
+                    <Button onClick={() => router.push('/calendarpage').then()}>Calendar Page</Button>
                 </div>
                 {/* <Card className="w-[350px]">
                     <CardHeader>
