5ad4609d5c4d6761612d0788bc3567cd4e7a8caf
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-10-11 12:47:36 -0700

    bye felecia

--- pages/secret.tsx
+++ pages/secret.tsx
@@ -1,15 +1,11 @@
 import {Inter} from 'next/font/google'
 
-import {useState} from 'react';
-import SecretSetting from './SecretSetting';
-import SecretProfile from './SecretProfile';
-
 const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
     return (
             <div>
-                
+
             </div>
 
     )
