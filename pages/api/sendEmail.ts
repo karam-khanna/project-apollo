f300a5b0add34c41ec034cec4350569e7450bc76
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-10-16 14:06:39 -0400

    Update sendEmail.ts

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -1,30 +1,29 @@
 import nodemailer from 'nodemailer';
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default async (req, res) => {
+export default async (req: any, res: any) => {
 
-  const { to, subject, message } = JSON.parse(req.body);
- 
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail', 
-    auth: {
-      user: process.env.EMAILUSER,
-      pass: process.env.EMAILPW,
-    },
-  });
+    const {to, subject, message} = JSON.parse(req.body);
 
-  const mailOptions = {
-    from: process.env.EMAILUSER,
-    to,
-    subject,
-    text: message,
-  };
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: process.env.EMAILUSER,
+            pass: process.env.EMAILPW,
+        },
+    });
 
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email sent' });
-  } catch (error) {
-    console.log('Error sending email:', error);
-    res.status(500).json({ error: 'Email not sent' });
-  }
+    const mailOptions = {
+        from: process.env.EMAILUSER,
+        to,
+        subject,
+        text: message,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({message: 'Email sent'});
+    } catch (error) {
+        console.log('Error sending email:', error);
+        res.status(500).json({error: 'Email not sent'});
+    }
 };
