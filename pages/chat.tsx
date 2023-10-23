9ed14c35db6210289c8b30fbbc9f4d9a3bb16f62
karam-khanna <84152938+karam-khanna@users.noreply.github.com>
2023-10-23 14:25:23 -0400

    updates

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,52 +1,52 @@
 "use client"
-import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced";
-import React, { useEffect, useState, useContext } from 'react'
-import { UserContext } from "@/context/UserContext";
-import { useRouter } from "next/router";
+import {useMultiChatLogic, MultiChatSocket, MultiChatWindow} from "react-chat-engine-advanced";
+import React, {useEffect, useState, useContext} from 'react'
+import {UserContext} from "@/context/UserContext";
+import {useRouter} from "next/router";
 import axios from 'axios';
 
 
 export default function Chat() {
-  const projectId = process.env.CHAT_PROJECT
+    const projectId = process.env.NEXT_PUBLIC_CHAT_PRIVATE
 
-  if (!projectId) {
-    throw new Error("no project id!")
-  }
-  const router = useRouter();
-  const { user, setUser } = useContext(UserContext);
-  const chatProps = useMultiChatLogic(projectId, user?.id || '', user?.email || '');
-  const [isReady, setReady] = useState(false);
-  useEffect(() => {
-    setReady(true)
-  }, [])
-  axios.put(
-    'https://api.chatengine.io/users/',
-    {
-      'username': user?.id,
-      'secret': user?.email
-    },
-    {
-      headers: {
-        'PRIVATE-KEY': process.env.CHAT_PRIVATE
-      }
+    if (!projectId) {
+        throw new Error("no project id!")
     }
-  ).then((response) => {
-    console.log(response.data)
-  }
-  ).catch(() => console.log("error")
-  );
-  const customRenderChatHeader = (props: { title?: React.ReactNode }) => {
-    return <div style={{
-      textAlign: 'center',
-      fontSize: 'larger',
-      fontWeight: 'bold'
-    }}>{props.title}</div>;
-  };
-  return isReady ? (
-    <div>
-      <MultiChatSocket {...chatProps} />
-      <MultiChatWindow {...chatProps} renderChatHeader={customRenderChatHeader} style={{ height: '100vh' }} />
-    </div>
-  ) : <div>errors...</div>
+    const router = useRouter();
+    const {user, setUser} = useContext(UserContext);
+    const chatProps = useMultiChatLogic(projectId, user?.id || '', user?.email || '');
+    const [isReady, setReady] = useState(false);
+    useEffect(() => {
+        setReady(true)
+    }, [])
+    axios.put(
+            'https://api.chatengine.io/users/',
+            {
+                'username': user?.id,
+                'secret': user?.email
+            },
+            {
+                headers: {
+                    'PRIVATE-KEY': process.env.NEXT_PUBLIC_CHAT_PROJECT
+                }
+            }
+    ).then((response) => {
+                console.log(response.data)
+            }
+    ).catch(() => console.log("error")
+    );
+    const customRenderChatHeader = (props: { title?: React.ReactNode }) => {
+        return <div style={{
+            textAlign: 'center',
+            fontSize: 'larger',
+            fontWeight: 'bold'
+        }}>{props.title}</div>;
+    };
+    return isReady ? (
+            <div>
+                <MultiChatSocket {...chatProps} />
+                <MultiChatWindow {...chatProps} renderChatHeader={customRenderChatHeader} style={{height: '100vh'}}/>
+            </div>
+    ) : <div>errors...</div>
 };
 
