6f9afbaf840580fb477185409184f3ac17c4ab50
justdoitel <127657148+justdoitel@users.noreply.github.com>
2023-11-06 00:11:05 -0500

    testing

---
+++ pages/api/sms.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import twilio from 'twilio';
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if(req.method == 'POST'){
+        const twilioMessage = await req.body.Body;
+        const senderPhoneNumber = await req.body.From;
+
+        const responseMessage = "Message received: " + twilioMessage;
+        
+        sendText (senderPhoneNumber, responseMessage);
+
+        const twilioResponse = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <Response>
+        <Message>${responseMessage}</Message>
+      </Response>
+    `;
+
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).end(twilioResponse)
+    }
+}
+
+const sendText = async (to: string, message: string) => {
+    try {
+        await client.messages.create({
+            body: message,
+            to, // The recipient's phone number
+            from: '+18559620462', // Your Twilio phone number
+          })
+         
+
+    } catch (error){
+        console.log('Error sending text:', error);
+      
+    }
+};
