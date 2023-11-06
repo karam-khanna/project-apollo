fedff46bf7f198593bfc59adfb4983997c6b988e
macarah <115976408+macarah@users.noreply.github.com>
2023-11-05 21:50:25 -0500

    Update sms.ts

--- pages/api/sms.ts
+++ pages/api/sms.ts
@@ -6,11 +6,20 @@ const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TO
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(req.method == 'POST'){
-        const twilioMessage = req.body.Body;
-        const senderPhoneNumber = req.body.From;
+        const twilioMessage = await req.body.Body;
+        const senderPhoneNumber = await req.body.From;
 
-        sendText (senderPhoneNumber, "message received that: "+twilioMessage);
+        const responseMessage = "Message received: " + twilioMessage;
+        
+        const twilioResponse = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <Response>
+        <Message>${responseMessage}</Message>
+      </Response>
+    `;
 
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).end(twilioResponse)
     }
 }
 
