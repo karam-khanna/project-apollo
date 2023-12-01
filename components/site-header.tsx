ca712b062e342570f28d75d136022d96006a3854
justdoitel <127657148+justdoitel@users.noreply.github.com>
2023-12-01 00:42:03 -0500

    make a bit more responsive

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -37,10 +37,11 @@ export function SiteHeader() {
         // You might want to return null or some default content when the condition is not met
         return <div />;
     }
+
     return (
         <header className="bg-background sticky top-0 z-40 w-full border-b">
             <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-                <MainNav items={siteConfig.mainNav} />
+                <MainNav />
                 <div className="flex flex-1 items-center justify-end space-x-4">
                     <nav className="flex items-center space-x-1">
                         {extras()}
@@ -66,4 +67,5 @@ export function SiteHeader() {
             </div>
         </header>
     )
+
 }
